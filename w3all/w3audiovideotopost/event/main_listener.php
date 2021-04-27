e59d552f85e30eeb89b72b216d048d8ee3a2b2fc
alessio nanni <alias@axew3.com>
2021-04-27 18:13:46 +0200

    Update main_listener.php

--- w3all/w3audiovideotopost/event/main_listener.php
+++ w3all/w3audiovideotopost/event/main_listener.php
@@ -68,56 +68,42 @@ class main_listener implements EventSubscriberInterface
 	{
 
    // usernames/postID pairs
-		foreach($e['rowset'] as $k => $v){
+		foreach($e['rowset'] as $k => $v)
+    {
 			$pidUA[$v['post_id']] = $v['username'];
 		}	
 
-		// if on viewtopic
-	if( strpos( $this->request->server('REQUEST_URI'), 'viewtopic.php') === false ){
+   // if on viewtopic
+   if( strpos( $this->request->server('REQUEST_URI'), 'viewtopic.'.$this->php_ext) === false ){
       $viewtopic = '';
     } else 
      {
-	    $viewtopic = 1;
+      $viewtopic = 1;
      }
-		
-		/*
-                    [attach_id] => 202
-                    [post_msg_id] => 16
-                    [topic_id] => 6
-                    [in_message] => 0
-                    [poster_id] => 2
-                    [is_orphan] => 0
-                    [physical_filename] => 2_s1_60f08ce9579ebf244074aac836ab6a13514613
-                    [real_filename] => 2_b520af4b.mp3
-                    [download_count] => 0
-                    [attach_comment] => 
-                    [extension] => mp3
-                    [mimetype] => audio/x-mpeg-3
-                    [filesize] => 21120
-                    [filetime] => 1613514613
-                    [thumbnail] => 0
-		*/
 	
 	 // extract all attachments for each post, and build a ready array of data to be used 
 	 // order attachments based on as they was, or as post_msg_id/attach_id
 	 // unset 'physical_filename' on resulting array
  
  // rebuild the same, but remove physical_filename
-	if( !empty($e['attachments']) ){
+   if( !empty($e['attachments']) ){
 
-	 	$posts_attachments_ary = $post_subary = array();
-	 	$i=0;
-	 	foreach( $e['attachments'] as $k => $v ){
-	   foreach( $v as $kk ){
-	    if( $kk['extension'] == 'mp3' && $kk['is_orphan'] == 0 ){
+      $posts_attachments_ary = $post_subary = array();
+      $i=0;
+      foreach( $e['attachments'] as $k => $v )
+	 {
+	   foreach( $v as $kk )
+	   {
+	    if( $kk['extension'] == 'mp3' && $kk['is_orphan'] == 0 )
+	    {
 	    	unset($kk['physical_filename']);
 	    	$post_subary[$i] = $kk;
 	    	$i++;
-       $posts_attachments_ary[$k] = $post_subary;
+                $posts_attachments_ary[$k] = $post_subary;
 	    }
-     }
-     $post_subary = array(); $i = 0;
-    }
+          }
+       $post_subary = array(); $i = 0;
+      }
 
    // Due to above. If the post contain more than an mp3 (or any other attach) the phpBB array come with attachments ordered 
    // with the last inserted, on index 0, the previous on 1 etc. This cause that a post that contain more than one attachment
@@ -126,10 +112,10 @@ class main_listener implements EventSubscriberInterface
    // Send the correct ordered array, also when a single post on topic contains more than one attachment
     foreach($posts_attachments_ary as $k => $a){
 	   if(count($a) > 1){ 
-		  $rev = array_reverse($a);
-		  $posts_attachments_ary[$k] = $rev;
-		 }
-    }
+	     $rev = array_reverse($a);
+	     $posts_attachments_ary[$k] = $rev;
+	   }
+     }
 
   if(!empty($posts_attachments_ary)){
    $posts_attachments_ary = json_encode($posts_attachments_ary,  JSON_FORCE_OBJECT | JSON_UNESCAPED_SLASHES);
@@ -145,6 +131,7 @@ class main_listener implements EventSubscriberInterface
 		 'W3ALL_AV_POST_ATTACHMENTS_ARY'	=> $posts_attachments_ary,
 		 'W3AVR_MODEON_VIEWTOPIC'	=> $viewtopic,
 		 'W3AVR_USERS_APOST_OWN'	=> $pidUA,
+		 'W3AVPHPBBEXT' => $this->php_ext,
     ));
 	 
   }
@@ -153,6 +140,7 @@ class main_listener implements EventSubscriberInterface
 
 	public function overall_footer_body_after()
 	{
+		//echo $this->config['max_filesize'];exit;
     $this->template->assign_vars(array( 
 		 'W3ALLREQ_MODE'	=> $this->request->variable('mode', ''),
 		 'W3ALLMAX_ATTACHDIM'	=> $this->config['max_filesize'],
