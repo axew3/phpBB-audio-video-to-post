71feb97a55d2bfa4061e3d47e163f2583dec1242
alessio nanni <alias@axew3.com>
2021-04-27 18:08:34 +0200

    Update common.php

--- w3all/w3audiovideotopost/language/en/common.php
+++ w3all/w3audiovideotopost/language/en/common.php
@@ -18,25 +18,13 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
-// DEVELOPERS PLEASE NOTE
-//
-// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
-//
-// Placeholders can now contain order information, e.g. instead of
-// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
-// translators to re-order the output of data while ensuring it remains correct
-//
-// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
-// equally where a string contains only two placeholders which are used to wrap text
-// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
-//
-// Some characters you may want to copy&paste:
-// ’ » “ ” …
-//
-
 $lang = array_merge($lang, array(
-
-	'W3AUDIOVIDEOTOPOST_HELLO'		=> 'Hello',
-	'W3AUDIOVIDEOTOPOST_GOODBYE'		=> 'Goodbye',
-
+	'W3AUDIOVIDEOTOPOST_STARTREC'	=> 'Start recording',
+	'W3AUDIOVIDEOTOPOST_STOPREC' => 'Stop recording',
+	'W3AUDIOVIDEOTOPOST_ONREC' => 'Recording',
+	'W3AUDIOVIDEOTOPOST_DELREC' => 'Delete',
+	'W3AUDIOVIDEOTOPOST_ANPAC' => 'attach name (optional)',
+	'W3AUDIOVIDEOTOPOST_EXPAC' => 'Only letters, numbers and - (hyphen)',	
+	'W3AUDIOVIDEOTOPOST_ATTACHA' => 'Attach audio',
+	'W3AUDIOVIDEOTOPOST_ATTACHV' => 'Attach video',
 ));
